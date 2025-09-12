e32c2a3ba752383aba461dfffb0e96eb37940947
Heitor Martini de Oliveira <martiniheitor@gmail.com>
2025-09-12 00:50:31 +0000

    Semana 05

--- flask_app.py
+++ flask_app.py
@@ -1,41 +1,37 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, request, url_for, render_template, flash, session, redirect
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Chave forte'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-# Home
-@app.route("/")
-def index():
-    current_time = datetime.utcnow()
-    return render_template("index.html", current_time=current_time)
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
-# Identificação
-@app.route("/user/<name>/<prontuario>/<instituicao>")
-def identificacao(name, prontuario, instituicao):
-    return render_template(
-        "user.html",
-        name=name,
-        prontuario=prontuario,
-        instituicao=instituicao,
-    )
+@app.route("/", methods=["GET", "POST"])
+def main():
+    form = NameForm()
 
-# Contexto da requisição
-@app.route("/contextorequisicao/<name>")
-def contexto(name):
-    user_agent = request.headers.get("User-Agent", "desconhecido")
-    remote_ip = request.remote_addr or "desconhecido"
-    host = request.host or "desconhecido"
-    return render_template(
-        "contexto.html",
-        name=name,
-        user_agent=user_agent,
-        remote_ip=remote_ip,
-        host=host,
-    )
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('main'))
+
+    return render_template('index.html', form=form, name=session.get('name'))
+
+# Handler para erros 404
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html'), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
