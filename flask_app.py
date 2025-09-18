108aa48611320a5a9ceb3ce9d703b8cd19681330
Heitor Martini de Oliveira <martiniheitor@gmail.com>
2025-09-18 23:14:19 +0000

    Semana 06

--- flask_app.py
+++ flask_app.py
@@ -1,36 +1,60 @@
-from flask import Flask, request, url_for, render_template, flash, session, redirect
-from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField, PasswordField
+from wtforms.validators import DataRequired
+from flask import Flask, render_template, session, redirect, url_for, flash, request
 from flask_moment import Moment
 from datetime import datetime
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Chave forte'
+app.config['SECRET_KEY'] = "Chave Forte"
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
+    name = StringField('Informe o seu nome:', validators = [DataRequired()])
+    last_name = StringField("Informe o seu sobrenome:", validators = [DataRequired()])
+    institution = StringField("Informe a sua Instituição e Ensino:", [DataRequired()])
+    course = SelectField("Informe a sua Disciplina:", choices=["DSWA5", "DWBA4", "Gestão de projetos"])
     submit = SubmitField('Submit')
 
-@app.route("/", methods=["GET", "POST"])
-def main():
-    form = NameForm()
+class LoginForm(FlaskForm):
+    username = StringField(validators = [DataRequired()])
+    password = PasswordField(validators = [DataRequired()])
+    submit = SubmitField('Submit')
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+            flash('Você alterou o seu nome.')
         session['name'] = form.name.data
-        return redirect(url_for('main'))
+        session['last_name'] = form.last_name.data
+        session['institution'] = form.institution.data
+        session['course'] = form.course.data
+        return redirect(url_for('index'))
+    remote_addr = request.remote_addr
+    host = request.host
+    print("Sessão atual: ", dict(session))
+    return render_template('index.html', form=form, name=session.get('name'), last_name=session.get('last_name'), institution=session.get('institution'), course=session.get('course'), remote_addr=remote_addr, host=host, current_time=datetime.utcnow())
 
-    return render_template('index.html', form=form, name=session.get('name'))
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST':
+        session['username'] = form.username.data
+        return redirect(url_for('login_response'))
+    return render_template('login.html', form=form, current_time=datetime.utcnow())
 
-# Handler para erros 404
+@app.route('/loginResponse')
+def login_response():
+    return render_template('login-response.html', username=session.get('username'), current_time=datetime.utcnow())
+
 @app.errorhandler(404)
 def page_not_found(e):
+    """Manipulador de erro para páginas não encontradas (404)."""
     return render_template('error.html'), 404
 
 if __name__ == "__main__":
