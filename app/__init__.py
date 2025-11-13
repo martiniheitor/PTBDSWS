c448d3e0b37e21ced1f9a0d1e83b409d764216a1
Heitor Martini de Oliveira <martiniheitor@gmail.com>
2025-11-13 22:42:30 +0000

    Semana 15 v2

--- app/__init__.py
+++ app/__init__.py
@@ -27,9 +27,13 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
 
+    # Importar views ANTES de registrar o blueprint
+    from .main import views
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    # Auth blueprint
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
