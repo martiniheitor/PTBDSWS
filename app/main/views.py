181997d8fa25212ae4109db0746e69a1f4d53d61
Heitor Martini de Oliveira <martiniheitor@gmail.com>
2025-11-21 00:46:24 +0000

    Semana 16

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,10 @@
 from flask import render_template
+from flask_login import login_required
 from . import main
-
+from app.models import User
 
 @main.route('/')
+@login_required
 def index():
-    return render_template('index.html')
+    usuarios = User.query.all()
+    return render_template('index.html', usuarios=usuarios)
